177620519476e2fb57ad0292fe0eb64c6e4b393a
meenakshirajput1 <mininice25.com>
2024-12-07 08:50:06 +0000

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 Developers are at work
 Application is ready  to be deployed to the development environment.
 development must be  reviewed by managers before deployment
+AWS CodeDeploy is a great tool.
